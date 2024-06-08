fe901266d0eccc949d037fcab1e845deb5ffd9ce
Artem Frolov <fartem2003@mail.ru>
2024-06-08 16:14:08 +0300

    fix

---
+++ blog/blog/settings/environments/production.py
@@ -0,0 +1,2 @@
+# common.py
+DEBUG = True
--- blog/run_uwsgi.sh
+++ blog/run_uwsgi.sh
@@ -6,7 +6,7 @@ chown www-data:www-data /var/log
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py compilemessages -l en -l ru
+# python manage.py compilemessages -l en -l ru
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
     python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" || true
